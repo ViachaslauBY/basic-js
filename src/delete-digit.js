4097f1e0ece5da0001e59c6a08b120e0dcbef6ff
Viachaslau Fedartsou <viachaslaufedartsou@gmail.com>
2023-04-12 21:46:03 +0300

    feat: solve delete digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,7 +1,15 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-function deleteDigit(/*n*/) {  
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {  
+  const numArr =  n.toString().split('').map(el => +el); 
+  let res = 0;
+  for (let i = 0; i < numArr.length; i++) {
+    let newArr = numArr.slice();
+    newArr.splice(i, 1);
+    if (res < Number(newArr.join('')))
+      res = Number(newArr.join(''));
+  }
+  return res;  
 }
 
 module.exports = {
